2de254f2b8d0cd2e0fa4d9d55c5ade91fe78f756
Takuto Yanagida <yanagida@stxst.com>
2023-04-10 23:51:14 +0900

    Improve gulp process

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2,18 +2,20 @@
  * Gulpfile
  *
  * @author Takuto Yanagida
- * @version 2023-03-25
+ * @version 2023-04-10
  */
 
 import gulp from 'gulp';
 
 import { makeJsTask } from './gulp/task-js.mjs';
+import { makeCopyTask } from './gulp/task-copy.mjs';
 
 const js = makeJsTask(['src/**/*.js', '!src/**/_*.js'], './dist', 'src');
+const copy = makeCopyTask('dist/*', './docs/lib');
 
 const watch = done => {
-	gulp.watch('src/**/*.js', js);
+	gulp.watch('src/**/*.js', gulp.series(js, copy));
 	done();
 };
 
-export default gulp.series(js, watch);
+export default gulp.series(js, copy, watch);
